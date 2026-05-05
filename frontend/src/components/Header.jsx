4e7927b2828a044cdca4dbac0717c501cb8d3d7f
readmaoo <you@example.com>
2026-05-05 17:21:17 +0500

    connect metamask

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,30 @@
 import "../styles/Header.css"
+import { ethers } from "ethers";
+import { useState } from "react";
 export default function Header(){
+    let signer = null;
+    let provider;
+    const [address, setAddress] = useState("");
+    async function ConnectMetamask(){
+        if (window.ethereum == null) {
+            console.log("MetaMask not installed; using read-only defaults")
+            provider = ethers.getDefaultProvider()
+        } else {
+            provider = new ethers.BrowserProvider(window.ethereum)
+            signer = await provider.getSigner();
+            const userAddress = await signer.getAddress();
+            setAddress(userAddress);
+        }
+    }
     return(
     <div className="Header">
     <h2 id="name">Simplified DAO Governance dApp frontend</h2>
-    <button id="metamask">CONNECT METAMASK</button>
+    <div className="auth-container">
+            <button id="metamask" onClick={ConnectMetamask}>
+                {address ? "CONNECTED" : "CONNECT METAMASK"}
+            </button>
+            {address && <p id="addr"> Your address: {address}</p>}
+        </div>
     </div>
     )
 }
